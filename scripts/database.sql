b254ed7b03f9220fd15af3981b5a9b3a44ff2825
dengzhiyong <dengzhiyong@cqgg.local>
2022-03-03 17:13:51 +0800

    add new table

--- scripts/database.sql
+++ scripts/database.sql
@@ -12,3 +12,11 @@ CREATE TABLE `clients` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='客户端';
 INSERT INTO `clients` (`id`, `name`, `secret`, `enabled`, `created_at`, `updated_at`) VALUES
 (1495691462048743424, 'beehive', 'GuVacPSuOwrPmjcAdWDvvkLR', 1, '2022-02-21 17:26:48', '2022-02-21 17:26:48');
+CREATE TABLE `topics` (
+  `id` bigint(20) unsigned NOT NULL COMMENT '数据表主键',
+  `name` varchar(64) NOT NULL COMMENT '名称',
+  `created_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_name` (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
